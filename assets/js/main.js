5ca0c9a4e735df791f9851db8553f4142fce0fba
laixuanthoi <laixuanthoi@gmail.com>
2021-01-15 02:38:07 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 4,
+      },
+    },
   });
+});
