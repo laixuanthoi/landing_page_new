17a743c3de8fdc72457e74c5d4c6ae7ea8c6f5cf
laixuanthoi <laixuanthoi@gmail.com>
2021-01-30 08:04:37 +0700

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,9 +24,12 @@ $(document).ready(function () {
   const link_val = extractUrlValue("link");
   // console.log(btn_val, link_val);
   const btn_uudai = $("#btn-uudai");
-
+  const btn_dangkyuudai_banner = $("#btn-dangkyuudai-banner");
   if (btn_val && link_val) {
     btn_uudai.attr("href", link_val);
     btn_uudai.text(btn_val);
+
+    btn_dangkyuudai_banner.attr("href", link_val);
+    btn_dangkyuudai_banner.text(btn_val);
   }
 });
